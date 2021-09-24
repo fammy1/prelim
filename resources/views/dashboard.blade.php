5e1dc0d166f024f905983499df09641a4f1610d4
dukey1221 <rubenestor816@gmail.com>
2021-09-24 22:52:33 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,60 @@
+@extends('base')
+
+@section('content')
+
+<style></style>
+
+  <div class="container">
+  <h1 class="title text-danger" >Welcome To IWatch  </h1>
+  </div>
+
+  <style>
+
+    .center {
+      padding: 70px 0;
+      border: 3px solid green;
+      text-align: center;
+      margin-top: 15%;
+    }
+    </style>
+    <br>
+    <br>
+    <div class="container">
+    
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="img5.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Money Heist</h5>
+                <p class="card-text">The group of thieves take hostages to aid in their negotiations with the authorities, who strategize to come up with a way to capture The Professor. the robbers prepare for a showdown with the police.</p>
+                <a href="#" class="btn btn-danger">Watch Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="img3.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Squid Game</h5>
+                <p class="card-text">Squid Game is a South Korean survival drama streaming television series written and directed by Hwang Dong-hyuk. Starring Lee Jung-jae, Park Hae-soo and Wi Ha-joon, tells the story of a group of people who risk their lives in a mysterious survival game with a ₩45.6 billion prize.</p>
+                <a href="#" class="btn btn-danger">Watch Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="img4.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Alice in Borderland</h5>
+                <p class="card-text">Obsessed gamer Arisu suddenly finds himself in a strange, emptied-out version of Tokyo in which he and his friends must compete in dangerous games in order to survive.</p>
+                <a href="#" class="btn btn-danger">Watch Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
+
